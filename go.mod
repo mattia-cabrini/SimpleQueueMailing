515b7b0e7a179dd2c18892970fe28af57f2c9fe1
Mattia Cabrini <dev@mattiacabrini.com>
2025-03-08 08:04:46 +0100

    Init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module SimpleMailer
+
+go 1.24.1
---
+++ main.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2023 Mattia Cabrini
+// SPDX-License-Identifier: MIT
+
+package main;
+
+func main() {
+	println("Hello World!")
+}
