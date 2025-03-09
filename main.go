2cc9c24acb3a18b2252c5a091b864a39ca527b95
Mattia Cabrini <dev@mattiacabrini.com>
2025-03-09 10:30:33 +0100

    Restructure / 2f

---
+++ main.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2023 Mattia Cabrini
+// SPDX-License-Identifier: MIT
+
+package main
+
+import (
+	"github.com/mattia-cabrini/SimpleQueueMailing/SimpleQueueMailing"
+)
+
+func main() {
+	SimpleQueueMailing.App()
+}
