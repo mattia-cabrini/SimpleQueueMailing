5ba136d65d84faeeeb45e35669dbd5df48c765a3
Mattia Cabrini <dev@mattiacabrini.com>
2026-01-02 15:06:06 +0100

    +Fix: ',' as separator and using 'Cc'

--- SimpleQueueMailing/message.go
+++ SimpleQueueMailing/message.go
@@ -138,8 +138,27 @@ func (m *message) Re() string {
 }
 
 func (m *message) To() (tos []string) {
-	toH := m.Header("To")
-	tos = strings.Split(toH, ";")
+	ToH := m.Header("To")
+	TOH := m.Header("TO")
+	CcH := m.Header("Cc")
+	CCH := m.Header("CC")
+
+	if ToH != "" {
+		tos = append(tos, strings.Split(ToH, ",")...)
+	}
+
+	if TOH != "" {
+		tos = append(tos, strings.Split(TOH, ",")...)
+	}
+
+	if CcH != "" {
+		tos = append(tos, strings.Split(CcH, ",")...)
+	}
+
+	if CCH != "" {
+		tos = append(tos, strings.Split(CCH, ",")...)
+	}
+
 	return
 }
 
--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/mattia-cabrini/SimpleQueueMailing
 go 1.24.1
 
 require (
-	github.com/mattia-cabrini/go-utility v0.0.8 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	github.com/mattia-cabrini/go-utility v0.0.8
+	gopkg.in/yaml.v3 v3.0.1
 )
